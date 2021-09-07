2036e1376fc14cedbef51c8d60e84455d2feb879
DCarr40 <davidwcarruthers@yahoo.com>
2021-09-06 22:10:19 -0400

    final touch ups

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const collections = require("./routes/collections");
-const bodyParser = require("body-parser");
 
 app.use(express.json());
 app.use(cors());
